7c7fb0b8e89457f5cb1b96f7eaa447158cd0ad00
Kelwin <kelwincomw@gmail.com>
2025-04-19 09:58:56 -0300

    add form

--- src/features/Auth/index.jsx
+++ src/features/Auth/index.jsx
@@ -2,11 +2,10 @@ import {Button} from '@/components/ui/button'
 
 export default function Auth() {
     return (
-        <div>
-            <h1>Auth</h1>
-            <Button>
-                Login
-            </Button>
+        <div className='flex flex-col items-center justify-center gap-4 p-8 text-center border-2 border-solid border-primary rounded-md'>
+            <h1 className='text-md font-bold'>
+                Bem vindo ao Dashboard Entrega Aqui
+            </h1>
         </div>
     )
 }
