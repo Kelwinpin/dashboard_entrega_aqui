b80f8633d68f3932c4a01a30a6aad891136adc41
Kelwinpin <kelwincomw@gmail.com>
2025-04-19 12:34:13 -0300

    add login screen

--- src/features/Auth/index.jsx
+++ src/features/Auth/index.jsx
@@ -1,11 +1,76 @@
-import {Button} from '@/components/ui/button'
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import loginSchema from '../../tools/schemas/login'
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { MaskInput } from "@/components/inputs/maskInput"
+import axios from "axios"
 
 export default function Auth() {
+    const form = useForm({
+        resolver: zodResolver(loginSchema),
+    })
+
+    async function onSubmit(values) {
+        values.cnpj = values.cnpj.replace(/\D/g, '');
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/login/dashboard`, values)
+        const data = response.data
+        console.log(data)
+    }
+
     return (
         <div className='flex flex-col items-center justify-center gap-4 p-8 text-center border-2 border-solid border-primary rounded-md'>
             <h1 className='text-md font-bold'>
                 Bem vindo ao Dashboard Entrega Aqui
             </h1>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="cnpj"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>CNPJ</FormLabel>
+                                <FormControl>
+                                    <MaskInput placeholder="CNPJ" dataMaska="000.000.000/0000-00" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="login"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Login</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Login" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Senha</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Senha" type="password" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
         </div>
     )
 }
