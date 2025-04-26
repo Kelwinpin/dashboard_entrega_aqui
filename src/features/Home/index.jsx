f22c38ff47b6fde60f9dc560f5868a63490cb21d
Kelwinpin <kelwincomw@gmail.com>
2025-04-26 11:47:33 -0300

    add home screen

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,9 +1,33 @@
+import home from "../../assets/home.svg"
+import { storage } from "@/tools/storage"
+
 export default function Home() {
+    const company = storage.getDecodedToken().company;
+    console.log("🚀 ~ Home ~ company:", company)
+
     return (
-        <div className="flex flex-col items-center justify-center gap-4 p-8 text-center border-2 border-solid border-primary rounded-md bg-white">
-            <h1 className="text-md font-medium">
-                Bem vindo ao Dashboard Entrega Aqui
-            </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-8 text-center rounded-md bg-white items-center justify-center">
+            <div>
+                <h1 className="text-md font-medium">
+                    Bem vindo ao Dashboard Entrega Aqui
+                </h1>
+                <p>
+                   Olá {company.name}
+                </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4 text-center rounded-md bg-white">
+                <img src={home} alt="home" className="h-96 w-full" />   
+            </div>
+            <div>
+                <h1 className="text-md font-medium">
+                    Últimos pedidos
+                </h1>
+            </div>
+            <div>
+                <h1 className="text-md font-medium">
+                    Últimos produtos cadastrados
+                </h1>
+            </div>
         </div>
     )
 }
--- src/tools/storage.js
+++ src/tools/storage.js
@@ -1,4 +1,6 @@
-function setToken(token) {
+import { jwtDecode } from "jwt-decode";
+
+  function setToken(token) {
     localStorage.setItem("auth_token", token);
   }
   
@@ -13,10 +15,19 @@ function setToken(token) {
   function getToken() {
     return localStorage.getItem("auth_token");
   }
+
+  function getDecodedToken() {
+    const token = getToken();
+    if (!token) {
+      return null;
+    }
+    return jwtDecode(token);
+  }
   
   export const storage = {
     setToken,
     hasToken,
     getToken,
     removeToken,
+    getDecodedToken,
   };
